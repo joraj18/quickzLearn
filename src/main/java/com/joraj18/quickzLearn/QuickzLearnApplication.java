76fe88d4ba00c56a0493f4d924d3ccf1e343298b
johith raj <johithraj18@gmail.com>
2025-04-20 12:59:53 +0530

    Initial Commit

---
+++ src/main/java/com/joraj18/quickzLearn/QuickzLearnApplication.java
@@ -0,0 +1,13 @@
+package com.joraj18.quickzLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickzLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickzLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joraj18/quickzLearn/QuickzLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joraj18.quickzLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickzLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
